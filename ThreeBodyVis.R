55dcd067ea7d6d8e0107817a7f4f097d1af7aaaf
wastjohn <st.john.will@gmail.com>
2023-12-07 16:20:50 -0600

    plot feature works but logic is flawed

--- ThreeBodyVis.R
+++ ThreeBodyVis.R
@@ -1,88 +1,244 @@
 heun_system_na_table <- function(
-    fxyt = function(x, y, t) {
+    fvx1 = function(v, t) {
 
     },
-    gxyt = function(x, y, t) {
+    fpx1 = function(x1, y1, x2, y2, x3, y3, t) {
 
     },
-    hxyt = function(x, y, t) {
+    fvy1 = function(v, t) {
 
     },
-    ixyt = function(x, y, t) {
+    fpy1 = function(x1, y1, x2, y2, x3, y3, t) {
 
     },
-    jxyt = function(x, y, t) {
+    fvx2 = function(v, t) {
 
     },
-    kxyt = function(x, y, t) {
+    fpx2 = function(x1, y1, x2, y2, x3, y3, t) {
 
     },
-    lxyt = function(x, y, t) {
+    fvy2 = function(v, t) {
 
     },
-    mxyt = function(x, y, t) {
+    fpy2 = function(x1, y1, x2, y2, x3, y3, t) {
 
     },
-    nxyt = function(x, y, t) {
+    fvx3 = function(v, t) {
 
     },
-    oxyt = function(x, y, t) {
+    fpx3 = function(x1, y1, x2, y2, x3, y3, t) {
 
     },
-    pxyt = function(x, y, t) {
+    fvy3 = function(v, t) {
 
     },
-    qxyt = function(x, y, t) {
+    fpy3 = function(x1, y1, x2, y2, x3, y3, t) {
 
     },
-    x10, y10, x20, y20, x30, y30, a, b, n) {
-    x <- 0
-    y <- 0
+    x10,
+    y10,
+    x20,
+    y20,
+    x30,
+    y30,
+    vx10,
+    vy10,
+    vx20,
+    vy20,
+    vx30,
+    vy30,
+    a,
+    b,
+    n) {
+    # initialize vector values
+    x1 <- 0
+    y1 <- 0
+    x2 <- 0
+    y2 <- 0
+    x3 <- 0
+    y3 <- 0
+    vx1 <- 0
+    vy1 <- 0
+    vx2 <- 0
+    vy2 <- 0
+    vx3 <- 0
+    vy3 <- 0
+
+    # initialize time
     t <- seq(a, b, by = (b - a) / (n - 1))
+
+    # initialize first values for each vector
     x1[1] <- x10
     x2[1] <- x20
     x3[1] <- x30
     y1[1] <- y10
     y2[1] <- y20
     y3[1] <- y30
+    vx1[1] <- vx10
+    vx2[1] <- vx20
+    vx3[1] <- vx30
+    vy1[1] <- vy10
+    vy2[1] <- vy20
+    vy3[1] <- vy30
 
+    # calculate Heun's method for each vector
     for (i in 2:n){
+        # Eulers method for body 1
+        # x-velocity, x-position, y-velocity, y-position
+        vx1_temp <- vx1[i - 1]
+            + fvx1(vx1[i - 1], t[i - 1]) * (b - a) / (n - 1)
         x1_temp <- x1[i - 1]
-            + fxyt(x1[i - 1], y1[i - 1], t[i - 1]) * (b - a) / (n - 1)
+            + fpx1(x1[i - 1], y1[i - 1],
+                x2[i - 1], y2[i - 1],
+                x3[i - 1], y3[i - 1],
+                t[i - 1]) * (b - a) / (n - 1)
+        vy1_temp <- vy1[i - 1]
+            + fvy1(vy1[i - 1], t[i - 1]) * (b - a) / (n - 1)
         y1_temp <- y1[i - 1]
-            + gxyt(x1[i - 1], y1[i - 1], t[i - 1]) * (b - a) / (n - 1)
+            + fpy1(y1[i - 1], x1[i - 1],
+                y2[i - 1], x2[i - 1],
+                y3[i - 1], x3[i - 1],
+                t[i - 1]) * (b - a) / (n - 1)
+
+        # Eulers method for body 2
+        # x-velocity, x-position, y-velocity, y-position
+        vx2_temp <- vx2[i - 1]
+            + fvx1(vx2[i - 1], t[i - 1]) * (b - a) / (n - 1)
         x2_temp <- x2[i - 1]
-            + hxyt(x2[i - 1], y2[i - 1], t[i - 1]) * (b - a) / (n - 1)
+            + fpx2(x2[i - 1], y2[i - 1],
+                x1[i - 1], y1[i - 1],
+                x3[i - 1], y3[i - 1],
+                t[i - 1]) * (b - a) / (n - 1)
+        vy2_temp <- vy2[i - 1]
+            + fvy2(vy2[i - 1], t[i - 1]) * (b - a) / (n - 1)
         y2_temp <- y2[i - 1]
-            + ixyt(x2[i - 1], y2[i - 1], t[i - 1]) * (b - a) / (n - 1)
+            + fpy2(y2[i - 1], x2[i - 1],
+                y1[i - 1], x1[i - 1],
+                y3[i - 1], x3[i - 1],
+                t[i - 1]) * (b - a) / (n - 1)
+
+        # Eulers method for body 3
+        # x-velocity, x-position, y-velocity, y-position
+        vx3_temp <- vx3[i - 1]
+            + fvx3(vx3[i - 1], t[i - 1]) * (b - a) / (n - 1)
         x3_temp <- x3[i - 1]
-            + jxyt(x3[i - 1], y3[i - 1], t[i - 1]) * (b - a) / (n - 1)
+            + fpx3(x3[i - 1], y3[i - 1],
+                x2[i - 1], y2[i - 1],
+                x1[i - 1], y1[i - 1],
+                t[i - 1]) * (b - a) / (n - 1)
+        vy3_temp <- vy3[i - 1]
+            + fvy3(vy3[i - 1], t[i - 1]) * (b - a) / (n - 1)
         y3_temp <- y3[i - 1]
-            + kxyt(x3[i - 1], y3[i - 1], t[i - 1]) * (b - a) / (n - 1)
+            + fpy3(y3[i - 1], x3[i - 1],
+                y2[i - 1], x2[i - 1],
+                y1[i - 1], x1[i - 1],
+                t[i - 1]) * (b - a) / (n - 1)
+
+        # Heuns method for body 1
+        # x-velocity, x-position, y-velocity, y-position
+        vx1[i] <- vx1[i - 1]
+            + (fvx1(vx1[i - 1], t[i - 1])
+            + fvx1(vx1_temp, t[i])) * (b - a) / (2 * (n - 1))
         x1[i] <- x1[i - 1]
-            + (fxyt(x1[i - 1], y1[i - 1], t[i - 1])
-            + fxyt(x1_temp, y1_temp, t[i])) * (b - a) / (2 * (n - 1))
+            + (fpx1(x1[i - 1], y1[i - 1],
+                    x2[i - 1], y2[i - 1],
+                    x3[i - 1], y3[i - 1],
+                    t[i - 1])
+            + fpx1(x1_temp, y1_temp,
+                    x2_temp, y2_temp,
+                    x3_temp, y3_temp,
+                    t[i])) * (b - a) / (2 * (n - 1))
+        vy1[i] <- vy1[i - 1]
+            + (fvy1(vy1[i - 1], t[i - 1])
+            + fvy1(vy1_temp, t[i])) * (b - a) / (2 * (n - 1))
         y1[i] <- y1[i - 1]
-            + (gxyt(x1[i - 1], y1[i - 1], t[i - 1])
-            + gxyt(x1_temp, y1_temp, t[i])) * (b - a) / (2 * (n - 1))
+            + (fpy1(y1[i - 1], x1[i - 1],
+                    y2[i - 1], x2[i - 1],
+                    y3[i - 1], x3[i - 1],
+                    t[i - 1])
+            + fpy1(y1_temp, x1_temp,
+                    y2_temp, x2_temp,
+                    y3_temp, x3_temp,
+                    t[i])) * (b - a) / (2 * (n - 1))
+
+
+        # Heuns method for body 2
+        # x-velocity, x-position, y-velocity, y-position
+        vx2[i] <- vx2[i - 1]
+            + (fvx2(vx2[i - 1], t[i - 1])
+            + fvx2(vx2_temp, t[i])) * (b - a) / (2 * (n - 1))
         x2[i] <- x2[i - 1]
-            + (hxyt(x2[i - 1], y2[i - 1], t[i - 1])
-            + hxyt(x2_temp, y2_temp, t[i])) * (b - a) / (2 * (n - 1))
+            + (fpx2(x2[i - 1], y2[i - 1],
+                    x1[i - 1], y1[i - 1],
+                    x3[i - 1], y3[i - 1],
+                    t[i - 1])
+            + fpx2(x2_temp, y2_temp,
+                    x1_temp, y1_temp,
+                    x3_temp, y3_temp,
+                    t[i])) * (b - a) / (2 * (n - 1))
+        vy2[i] <- vy2[i - 1]
+            + (fvy2(vy2[i - 1], t[i - 1])
+            + fvy2(vy2_temp, t[i])) * (b - a) / (2 * (n - 1))
         y2[i] <- y2[i - 1]
-            + (ixyt(x2[i - 1], y2[i - 1], t[i - 1])
-            + ixyt(x2_temp, y2_temp, t[i])) * (b - a) / (2 * (n - 1))
+            + (fpy2(y2[i - 1], x2[i - 1],
+                    y1[i - 1], x1[i - 1],
+                    y3[i - 1], x3[i - 1],
+                    t[i - 1])
+            + fpy2(y2_temp, x2_temp,
+                    y1_temp, x1_temp,
+                    y3_temp, x3_temp,
+                    t[i])) * (b - a) / (2 * (n - 1))
+
+        # Heuns method for body 1
+        # x-velocity, x-position, y-velocity, y-position
+        vx3[i] <- vx3[i - 1]
+            + (fvx3(vx3[i - 1], t[i - 1])
+            + fvx3(vx3_temp, t[i])) * (b - a) / (2 * (n - 1))
         x3[i] <- x3[i - 1]
-            + (jxyt(x3[i - 1], y1[i - 1], t[i - 1])
-            + jxyt(x3_temp, y3_temp, t[i])) * (b - a) / (2 * (n - 1))
+            + (fpx3(x3[i - 1], y3[i - 1],
+                    x2[i - 1], y2[i - 1],
+                    x1[i - 1], y1[i - 1],
+                    t[i - 1])
+            + fpx3(x3_temp, y3_temp,
+                    x2_temp, y2_temp,
+                    x1_temp, y1_temp,
+                    t[i])) * (b - a) / (2 * (n - 1))
+        vy3[i] <- vy3[i - 1]
+            + (fvy3(vy3[i - 1], t[i - 1])
+            + fvy3(vy3_temp, t[i])) * (b - a) / (2 * (n - 1))
         y3[i] <- y3[i - 1]
-            + (kxyt(x3[i - 1], y3[i - 1], t[i - 1])
-            + kxyt(x3_temp, y3_temp, t[i])) * (b - a) / (2 * (n - 1))
+            + (fpy3(y3[i - 1], x3[i - 1],
+                    y2[i - 1], x2[i - 1],
+                    y1[i - 1], x1[i - 1],
+                    t[i - 1])
+            + fpy3(y3_temp, x3_temp,
+                    y2_temp, x2_temp,
+                    y1_temp, x1_temp,
+                    t[i])) * (b - a) / (2 * (n - 1))
     }
-    plot(x, y, col = "red", pch = 20, cex = 1.5)
-    points(x1, y1, col = "red", pch = 20, cex = 2.5)
-    points(x2[1], y2[1], col = "green", pch = 20, cex = 2.5)
-    points(x3[1], y3[1], col = "blue", pch = 20, cex = 2.5)
+    plot(x1, y1, col = "red", pch = 20, cex = 2.5)
+    points(x2, y2, col = "green", pch = 20, cex = 2.5)
+    points(x3, y3, col = "blue", pch = 20, cex = 2.5)
     recordPlot()
-    table <- data.frame(t, x, y)
+    table <- data.frame(t, x1, y1, x2, y2, x3, y3)
     return(table)
 }
+
+velocity <- function(v, t) {
+    v
+}
+
+position <- function(x1, y1, x2, y2, x3, y3, t) {
+    -(x1 - x2 / ((x1 - x2)^2 + (y1 - y2)^2)^(3 / 2))
+    - (x1 - x3 / ((x1 - x3)^2 + (y1 - y3)^2)^(3 / 2))
+}
+
+heun_table <- heun_system_na_table(velocity, position,
+                    velocity, position,
+                    velocity, position,
+                    velocity, position,
+                    velocity, position,
+                    velocity, position,
+                    1, 1, 2, 2, 3, 3,
+                    0, 0, 0, 0, 0, 0,
+                    0, 60, 601)
