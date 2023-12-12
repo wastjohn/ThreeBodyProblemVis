943a432aa7c6ad75513ce259fbcd4b194a926401
km-phys <kmurphy1@macalester.edu>
2023-12-12 16:03:15 -0600

    masses

---
+++ Python_code_testing/Mass Experiments.py
@@ -0,0 +1,89 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.animation as animation
+
+G = 6.67430 * (10 ** (-11))
+print(G)
+
+def heun(p0: np.array, m:np.array, N: int, t: float) -> np.array:
+    """Calculatue heuns method for the three body problem for N steps with step size t.
+
+    Args:
+        p0 (np.array): 12 dimensional vector of initial conditions corresponding to the following:
+            p0[0] = x1, p0[1] = x2, p0[2] = x3, p0[3] = y1, p0[4] = y2, p0[5] = y3, 
+            p0[6] = vx1, p0[7] = vx2, p0[8] = vx3, p0[9] = vy1, p0[10] = vy2, p0[11] = vy3
+        m (np.array): 3 dimensional vector with masses corresponding to the following: 
+            m[0]= m1, m[1] = m2, m[2] = m3
+        N (int): number of steps
+        t (float): step size
+
+    Returns:
+        np.array: np.array positions and velocities for each x and y of each planet
+    """
+    data = [p0[0], p0[1], p0[2], p0[3], p0[4], p0[5], 
+            p0[6], p0[7], p0[8], p0[9], p0[10], p0[11], 
+            derivative(p0)[6], derivative(p0)[7], derivative(p0)[8], 
+            derivative(p0)[9], derivative(p0)[10], derivative(p0)[11]]
+    df = pd.DataFrame(data=[data], columns=['P1: X Position', 'P2: X Position', 
+                                'P3: X Position', 'P1: Y Position', 'P2: Y Position', 
+                                'P3: Y Position', 'P1: X Velocity', 'P2: X Velocity', 
+                                'P3: X Velocity', 'P1: Y Velocity', 'P2: Y Velocity', 
+                                'P3: Y Velocity', 'P1: X Acceleration', 'P2: X Acceleration', 
+                                'P3: X Acceleration', 'P1: Y Acceleration', 'P2: Y Acceleration', 
+                                'P3: Y Acceleration'])
+    
+    # TODO: verify times and step sizes are correct
+    for i in range(0, N):
+        ptemp = p0 + t * derivative(p0)
+        dp0 = derivative(p0)
+        dptemp = derivative(ptemp)
+        p0 = p0 + t/2 * (dp0 + dptemp)
+        data = [p0[0], p0[1], p0[2], p0[3], p0[4], p0[5], p0[6], p0[7], p0[8], p0[9], p0[10], p0[11], 
+            derivative(p0)[6], derivative(p0)[7], derivative(p0)[8], derivative(p0)[9], derivative(p0)[10], 
+            derivative(p0)[11]]
+        df2 = pd.DataFrame(data=[data], columns=['P1: X Position', 'P2: X Position', 
+                                'P3: X Position', 'P1: Y Position', 'P2: Y Position', 
+                                'P3: Y Position', 'P1: X Velocity', 'P2: X Velocity', 
+                                'P3: X Velocity', 'P1: Y Velocity', 'P2: Y Velocity', 
+                                'P3: Y Velocity', 'P1: X Acceleration', 'P2: X Acceleration', 
+                                'P3: X Acceleration', 'P1: Y Acceleration', 'P2: Y Acceleration', 
+                                'P3: Y Acceleration'])
+        df2['P1: X Position'] = p0[0]
+        df2['P2: X Position'] = p0[1]
+        df2['P3: X Position'] = p0[2]
+        df2['P1: Y Position'] = p0[3]
+        df2['P2: Y Position'] = p0[4]
+        df2['P3: Y Position'] = p0[5]
+        df2['P1: X Velocity'] = p0[6]
+        df2['P2: X Velocity'] = p0[7]
+        df2['P3: X Velocity'] = p0[8]
+        df2['P1: Y Velocity'] = p0[9]
+        df2['P2: Y Velocity'] = p0[10]
+        df2['P3: Y Velocity'] = p0[11]
+        df = pd.concat([df, df2], ignore_index=True)
+    
+    df.insert(0, 'Time', [i * t for i in range(0, N + 1)])
+    return df
+
+
+def derivative(p0: np.array):
+    p1 = 0 * p0
+    p1[0] = p0[6]
+    p1[1] = p0[7]
+    p1[2] = p0[8]
+    p1[3] = p0[9]
+    p1[4] = p0[10]
+    p1[5] = p0[11]
+    p1[6] = - (G*m[1]*(p0[0] - p0[1])/((p0[0]-p0[1])**2+(p0[3]-p0[4])**2)**(3/2)) - (G*m[2]*(p0[0] - p0[2])/((p0[0]-p0[2])**2+(p0[3]-p0[5])**2)**(3/2))
+    p1[7] = - (G*m[0]*(p0[1] - p0[0])/((p0[1]-p0[0])**2+(p0[4]-p0[3])**2)**(3/2)) - (G*m[2]*(p0[1] - p0[2])/((p0[1]-p0[2])**2+(p0[4]-p0[5])**2)**(3/2))
+    p1[8] = - (G*m[0]*(p0[2] - p0[0])/((p0[2]-p0[0])**2+(p0[5]-p0[3])**2)**(3/2)) - (G*m[1]*(p0[2] - p0[1])/((p0[2]-p0[1])**2+(p0[5]-p0[4])**2)**(3/2))
+    p1[9] = - (G*m[1]*(p0[3] - p0[4])/((p0[0]-p0[1])**2+(p0[3]-p0[4])**2)**(3/2)) - (G*m[2]*(p0[3] - p0[5])/((p0[0]-p0[2])**2+(p0[3]-p0[5])**2)**(3/2))
+    p1[10] = - (G*m[0]*(p0[4] - p0[3])/((p0[1]-p0[0])**2+(p0[4]-p0[3])**2)**(3/2)) - (G*m[2]*(p0[4] - p0[5])/((p0[1]-p0[2])**2+(p0[4]-p0[5])**2)**(3/2))
+    p1[11] = - (G*m[0]*(p0[5] - p0[3])/((p0[2]-p0[0])**2+(p0[5]-p0[3])**2)**(3/2)) - (G*m[1]*(p0[5] - p0[4])/((p0[2]-p0[1])**2+(p0[5]-p0[4])**2)**(3/2))
+    return p1
+
+
+
+
